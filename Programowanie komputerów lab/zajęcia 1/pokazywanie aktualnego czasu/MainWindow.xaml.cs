a0c8f4c36a99efca528b2dbe13c78ae69586a669
Marek <61633829+marek-kaminski@users.noreply.github.com>
2021-11-01 21:19:00 +0100

    Add files via upload

---
+++ Programowanie komputerów lab/zajęcia 1/pokazywanie aktualnego czasu/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            naszaKontrolka.Content = "Hello world!";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            naszaKontrolka.Content = "Witaj świecie";
+            //naszaKontrolka.Content = "Witaj świecie";
+        }
+
+        private void lblCzas_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DateTime czas = DateTime.Now;
+            lblCzas.Content = czas.ToString("T");
+        }
+
+        private void lblCzas_MouseLeave(object sender, MouseEventArgs e)
+        {
+            lblCzas.Content = "Czas";
+        }
+    }
+}
